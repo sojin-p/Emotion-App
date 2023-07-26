25506ef6a102209220f1d55c0516e95b6ddef74c
sojin-p <luv2hong@gmail.com>
2023-07-26 15:48:17 +0900

    버튼 클릭하면 클릭 수 증가 - 단계별로 Enum 적용

--- Emotion App/ViewController.swift
+++ Emotion App/ViewController.swift
@@ -23,9 +23,72 @@ class ViewController: UIViewController {
     
     //버튼을 누르면, 클릭 수가 증가
     @IBAction func emotionButton(_ sender: UIButton) {
+        
+        //차근 차근 Enum적용 해보기
+        //3번 열거형(Enum)의 rawValue 활용
+        guard let value = Emotion(rawValue: sender.tag) else {
+            print("오류")
+            return
+        }
+        
+        let tag = sender.tag
+        buttonCount[tag] += 1
+        
+        switch value {
+        case .완전행복: print("\(buttonCount[tag])점")
+        case .적당미소: print("\(buttonCount[tag])점")
+        case .그냥그냥: print("\(buttonCount[tag])점")
+        case .좀속상한: print("\(buttonCount[tag])점")
+        case .많이슬픈: print("\(buttonCount[tag])점")
+        }
+        
+        //2번 스위치로 바꿔보기
+//        let tag = sender.tag
+//        switch tag {
+//        case Emotion.완전행복.rawValue:
+//            buttonCount[tag] = buttonCount[tag] + 1
+//            print("\(buttonCount[tag])점")
+//        case Emotion.적당미소.rawValue:
+//            buttonCount[tag] = buttonCount[tag] + 1
+//            print("\(buttonCount[tag])점")
+//        case Emotion.그냥그냥.rawValue:
+//            buttonCount[tag] = buttonCount[tag] + 1
+//            print("\(buttonCount[tag])점")
+//        case Emotion.좀속상한.rawValue:
+//            buttonCount[tag] = buttonCount[tag] + 1
+//            print("\(buttonCount[tag])점")
+//        case Emotion.많이슬픈.rawValue:
+//            buttonCount[tag] = buttonCount[tag] + 1
+//            print("\(buttonCount[tag])점")
+//        default:
+//            print("오류")
+//        }
+        
+        //1번 조건문
+//        let tag = sender.tag
+//        if tag == Emotion.완전행복.rawValue {
+//            buttonCount[tag] = buttonCount[tag] + 1
+//            print("\(buttonCount[tag])점")
+//        } else if tag == Emotion.적당미소.rawValue {
+//            buttonCount[tag] = buttonCount[tag] + 1
+//            print("\(buttonCount[tag])점")
+//        } else if tag == Emotion.그냥그냥.rawValue {
+//            buttonCount[tag] = buttonCount[tag] + 1
+//            print("\(buttonCount[tag])점")
+//        } else if tag == Emotion.좀속상한.rawValue {
+//            buttonCount[tag] = buttonCount[tag] + 1
+//            print("\(buttonCount[tag])점")
+//        } else if tag == Emotion.많이슬픈.rawValue {
+//            buttonCount[tag] = buttonCount[tag] + 1
+//            print("\(buttonCount[tag])점")
+//        } else {
+//            print("오류")
+//        }
                 
-        buttonCount[sender.tag] = buttonCount[sender.tag] + 1
-        print(buttonCount[sender.tag])
+        
+//        //기존에 쓴 코드
+//        buttonCount[sender.tag] += 1
+//        print(buttonCount[sender.tag])
         
     }
     
