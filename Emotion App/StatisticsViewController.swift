a6747c02ffbd3177e22eaa5599a6109a995512ad
sojin-p <luv2hong@gmail.com>
2023-07-26 21:43:52 +0900

    결과 Lebel 색상 변경

--- Emotion App/StatisticsViewController.swift
+++ Emotion App/StatisticsViewController.swift
@@ -11,6 +11,7 @@ class StatisticsViewController: UIViewController {
 
     @IBOutlet var backgroundView: [UIView]!
     @IBOutlet var titleLabels: [UILabel]!
+    @IBOutlet var resultLabels: [UILabel]!
     
     let colorList: [UIColor] = [.systemPink, .orange, .yellow, .green, .blue]
     
@@ -19,15 +20,28 @@ class StatisticsViewController: UIViewController {
         
         designView()
         designTitleLabel()
-
+        designResultLabel()
+        
+    }
+    
+    func designResultLabel() {
+        for (index, resultLabel) in resultLabels.enumerated() {
+            switch index {
+            case 0, 4: resultLabel.textColor = .white
+            default:
+                resultLabel.textColor = .black
+            }
+            resultLabel.text = "점"
+            resultLabel.textAlignment = .right
+            resultLabel.font = UIFont.systemFont(ofSize: 30)
+        }
     }
 
     func designTitleLabel() {
         for (index, title) in titleLabels.enumerated() {
             switch title.tag {
             case 0, 4: title.textColor = .white
-            default:
-                title.textColor = .black
+            default: title.textColor = .black
             }
             title.text = "\(emotionList[index])지수"
         }
